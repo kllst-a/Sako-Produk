40516e853173fbcadea1a24a0704ca6185d62a94
Kllst <andreanecallista@gmail.com>
2025-05-10 09:52:41 +0700

    create branch

---
+++ SakoKelolaProduk/SakoKelolaProduk/SakoKelolaProdukApp.swift
@@ -0,0 +1,17 @@
+//
+//  SakoKelolaProdukApp.swift
+//  SakoKelolaProduk
+//
+//  Created by Callista on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SakoKelolaProdukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
